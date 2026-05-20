dd45ec4902de895260bf8436599508651e9a59ed
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-19 23:41:28 -0400

    Update Nautilus requirement

--- ECCLibrary/ECCLibrary/Plugin/ECCPlugin.cs
+++ ECCLibrary/ECCLibrary/Plugin/ECCPlugin.cs
@@ -3,7 +3,7 @@
 namespace ECCLibrary;
 
 [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-[BepInDependency("com.snmodding.nautilus", "1.0.0.42")]
+[BepInDependency("com.snmodding.nautilus", "1.0.0.50")]
 internal class ECCPlugin : BaseUnityPlugin
 {
     public static ECCConfig config = OptionsPanelHandler.RegisterModOptions<ECCConfig>();
