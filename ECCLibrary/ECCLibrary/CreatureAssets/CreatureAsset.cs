b48e24bd20e39d4927ec9d9d8860b4ae9bd6cc18
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-09-15 18:32:34 -0400

    Fix creatures with same TechType lacking WEI

--- ECCLibrary/ECCLibrary/CreatureAssets/CreatureAsset.cs
+++ ECCLibrary/ECCLibrary/CreatureAssets/CreatureAsset.cs
@@ -96,14 +96,11 @@ public partial class CreatureAsset
 
         if (registeringTechTypeForFirstTime)
         {
-            if (Template.AcidImmune) CreatureDataUtils.SetAcidImmune(TechType);
-            if (Template.BioReactorCharge > 0f) CreatureDataUtils.SetBioreactorCharge(TechType, Template.BioReactorCharge);
-            if (Template.PickupableFishData != null && Template.PickupableFishData.CanBeHeld) CraftDataHandler.SetEquipmentType(TechType, EquipmentType.Hand);
-            CreatureDataUtils.SetBehaviorType(TechType, Template.BehaviourType);
-            CreatureDataUtils.SetItemSounds(TechType, Template.ItemSoundsType);
-            EntityInfo = new UWE.WorldEntityInfo { cellLevel = Template.CellLevel, classId = ClassID, localScale = Vector3.one, prefabZUp = false, slotType = EntitySlot.Type.Creature, techType = TechType };
-            WorldEntityDatabaseHandler.AddCustomInfo(ClassID, EntityInfo);
+            RegisterTechTypeData();
         }
+        
+        EntityInfo = new UWE.WorldEntityInfo { cellLevel = Template.CellLevel, classId = ClassID, localScale = Vector3.one, prefabZUp = false, slotType = EntitySlot.Type.Creature, techType = TechType };
+        WorldEntityDatabaseHandler.AddCustomInfo(ClassID, EntityInfo);
 
         // Register the Custom Prefab
 
@@ -122,6 +119,15 @@ public partial class CreatureAsset
         CustomPrefabInstance.Register();
 
         PostRegister();
+    }
+
+    private void RegisterTechTypeData()
+    {
+        if (Template.AcidImmune) CreatureDataUtils.SetAcidImmune(TechType);
+        if (Template.BioReactorCharge > 0f) CreatureDataUtils.SetBioreactorCharge(TechType, Template.BioReactorCharge);
+        if (Template.PickupableFishData != null && Template.PickupableFishData.CanBeHeld) CraftDataHandler.SetEquipmentType(TechType, EquipmentType.Hand);
+        CreatureDataUtils.SetBehaviorType(TechType, Template.BehaviourType);
+        CreatureDataUtils.SetItemSounds(TechType, Template.ItemSoundsType);
     }
 
     /// <summary>
